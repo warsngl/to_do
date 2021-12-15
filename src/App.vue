8f62b113344702321543e914525b7feecd62a997
warsngl <41682433+warsngl@users.noreply.github.com>
2021-12-15 19:17:39 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+<h1>Template 4 Vite</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
